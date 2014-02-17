a39d19c5e8cc521a267fcd2b195ddb5c39a6c443
Marta Proksova <sojka@m-atelier.cz>
2014-02-17 10:38:40 +0100

    - tests

---
+++ profique/src/test/java/ma/demo/devfest/profique/DummyTest.java
@@ -0,0 +1,16 @@
+package ma.demo.devfest.profique;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by stepan on 16.2.14.
+ */
+public class DummyTest {
+
+    @Test()
+    public void assertEqualsAddition() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
